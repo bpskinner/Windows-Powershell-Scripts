0bd739f64e64141fd627e10a2dba742b38468360
Benjamin P. Skinner <68821301+bpskinner@users.noreply.github.com>
2025-01-06 08:46:42 -0500

    Added scripts to Local_Machine folder

---
+++ Local_Machine/Change_To_DHCP .ps1
@@ -0,0 +1,56 @@
+#!PS
+## Get the Physical NIC
+$adapter = Get-NetAdapter -Physical
+
+## Get the NIC Name as a System Variable
+$nicName = $Adapter.Name
+
+## Get the NIC Index
+$nicIndex = $adapter.ifIndex
+
+## Set the network interface to a variable for future use
+$interface = Get-NetIPInterface -InterfaceIndex $nicIndex -AddressFamily IPv4
+
+## Remove the static default gateway
+Remove-NetRoute -InterfaceIndex $nicIndex -AddressFamily IPv4 -Confirm:$false
+
+## Set interface to "Obtain an IP address automatically"
+Set-NetIPInterface -InterfaceIndex $nicIndex -Dhcp Enabled
+
+## Set interface to "Obtain DNS server address automatically"
+$interface | Set-DnsClientServerAddress -ResetServerAddresses
+
+## Sleep to allow some environments to process (older hardware)
+Start-Sleep -Seconds 10
+
+## Restart the NIC for Sanity
+Restart-NetAdapter -Name $nicName -Confirm:$false -Verbose
+
+## Sleep to allow some environments to process (older hardware)
+Start-Sleep -Seconds 10
+
+## Display new IP 
+### (nice for me as I utilize this information but not needed)
+$NewIP = (Get-NetIPAddress -InterfaceIndex $nicIndex).IPv4Address
+$NewIP
+
+## Register the adapter in DNS
+Register-DnsClient -Verbose
+
+## Flush the DNS cache
+Clear-DnsClientCache -Verbose
+
+
+
+#!PS
+# Reconfigure STATIC gateway
+
+## Get the NIC Index
+$nicIndex = (get-netroute | ? {$_.NextHop -eq '10.226.138.254'}).ifIndex
+if ($nicIndex -ne $null) {
+    New-NetRoute -DestinationPrefix 0.0.0.0/0 -NextHop 10.226.138.224 -InterfaceIndex $nicIndex -AddressFamily IPv4 -RouteMetric 1 -Verbose
+    Sleep 15
+    if ([Boolean](Test-Connection -IPAddress '1.1.1.1' -Count 3 -ErrorAction SilentlyContinue)) {
+        Remove-NetRoute -InterfaceIndex $nicIndex -NextHop '10.226.138.254' -Confirm:$false -Verbose
+        }
+    }
---
+++ Local_Machine/Move_to_SSID.ps1
@@ -0,0 +1,154 @@
+﻿#!PS
+$SSID         = "Example-Corp" # Case sensitive
+$password     = ""
+$SKIP_THESE   = "Example1_SSID","Example2_SSID" # If connected to these SSID's, do not run script.
+$REMOVE_THESE = "Example-GUEST" # Removed AND Hides the network. This is a REGEX match, meaning anything you type will be matched against ANY possible matches.
+$FORCE        = $true # force update/join to SSID regardless of hardwired/wifi status.
+$HIDEALL      = $false # If true, hide ALL other SSID's except the one defined in $SSID
+
+
+function change_SSID {
+    $password = $password -replace "&","&amp;"
+    $Current_SSID = (Get-CurrentWLAN).SSID
+    
+    if ($SKIP_THESE -contains $Current_SSID) { 
+    
+        Write-host "CONNECTED TO $($Current_SSID), SKIPPING!" 
+    
+    } else { 
+        if (-not $FORCE) {
+            $using_ETHERNET = $false
+            $using_WIFI = $false
+
+            Get-NetAdapter | Foreach-object {
+                if ($_.Status -eq 'Up' -and $_.MediaType -match '802.3') { 
+                    Write-host "CONNECTED VIA ETHERNET > SKIPPING"
+                    Exit 
+                }
+            }
+
+            if (-not $using_ETHERNET) {
+                $WirelessAdapter = Get-NetAdapter | ? {
+                    ($_.Status -eq 'Up' -and $_.MediaType -match '802.11' -and ($_.InterfaceDescription -match "Wi[\-]{0,1}Fi|Wireless" -or $_.Name -match "Wi[\-]{0,1}Fi|Wireless"))
+                }
+                
+                if ($WirelessAdapter) { 
+                    Write-host Wireless adapter found `"$($WirelessAdapter.Name) / $($WirelessAdapter.InterfaceDescription)`"
+                    $using_WIFI = $true 
+                }
+            }
+        }
+        
+        if ($FORCE) { 
+            $WirelessAdapter = Get-NetAdapter | ? {
+                ($_.InterfaceDescription -match "Wi[\-]{0,1}Fi|Wireless" -or $_.Name -match "Wi[\-]{0,1}Fi|Wireless")
+            }
+            if ($WirelessAdapter.count -gt 1) { $WirelessAdapter = $WirelessAdapter | ? {$_.Status -eq 'Up'} }
+            Write-host Wireless adapter found `"$($WirelessAdapter.Name) / $($WirelessAdapter.InterfaceDescription)`"
+            $using_WIFI = $true 
+        }
+    
+        if ($Current_SSID -notmatch $SSID -and $using_WIFI -eq $true) {
+            Write-host "Attempting connection to `"$SSID`""
+    
+            $hex = (Format-Hex -InputObject $SSID -Encoding ascii).ToString().replace('00000000','').replace($SSID,'').trim().replace(' ','')
+
+            $currentprofiles=(((netsh.exe wlan show profiles) -match '\s{2,}:\s').split([Environment]::NewLine) | % {$_.split(':')[1].trim()}) | ? {$_ -notmatch $SSID}
+
+            $xml_header = '<?xml version="1.0"?>
+            <WLANProfile xmlns="http://www.microsoft.com/networking/WLAN/profile/v1">
+            '
+            $xml_body = `
+            "	<name>$($SSID)</name>
+	            <SSIDConfig>
+		            <SSID>
+			            <hex>$($hex)</hex>
+			            <name>$($SSID)</name>
+		            </SSID>
+	            </SSIDConfig>
+	            <connectionType>ESS</connectionType>
+	            <connectionMode>auto</connectionMode>
+	            <MSM>
+		            <security>
+			            <authEncryption>
+				            <authentication>WPA2PSK</authentication>
+				            <encryption>AES</encryption>
+				            <useOneX>false</useOneX>
+			            </authEncryption>
+			            <sharedKey>
+				            <keyType>passPhrase</keyType>
+				            <protected>false</protected>
+				            <keyMaterial>$($password)</keyMaterial>
+			            </sharedKey>
+		            </security>
+	            </MSM>
+            "
+            $xml_trailer =`
+            '	<MacRandomization xmlns="http://www.microsoft.com/networking/WLAN/profile/v3">
+		            <enableRandomization>false</enableRandomization>
+	            </MacRandomization>
+            </WLANProfile>'
+
+            ($xml_header + $xml_body + $xml_trailer) > "c:\users\public\SSIDProfile.xml"
+
+            Netsh WLAN add profile filename="c:\users\public\SSIDProfile.xml"
+            sleep 3
+            Netsh WLAN connect name="$($SSID)" interface="$($WirelessAdapter.Name)"
+            Remove-item "c:\users\public\SSIDProfile.xml" -Force
+        } else {
+            Write-host "ALREADY CONNECTED TO > $SSID"
+        }
+    }
+}
+
+function cleanup_profiles {
+	if ($REMOVE_THESE -eq $null) { return }
+    $GuestProfiles=(((netsh.exe wlan show profiles) -match '\s{2,}:\s').split([Environment]::NewLine) | % {$_.split(':')[1].trim()}) | ? {$_ -imatch $REMOVE_THESE}
+    $GuestProfiles
+    $GuestProfiles | % { 
+        Netsh wlan delete profile $_ 
+        Netsh wlan add filter permission=block ssid="$_" networktype=infrastructure
+    }
+    if ($HIDEALL) {
+        netsh wlan add filter permission=allow ssid="$SSID" networktype=infrastructure
+        netsh wlan add filter permission=denyall networktype=infrastructure 
+    }
+    
+    netsh wlan show filters
+    
+}
+
+function Get-CurrentWLAN {
+
+    # Get WLAN interface information
+    $netshOutput = netsh wlan show interfaces
+    $lines = $netshOutput -split "\r`n"`
+
+    # Create an object to store WLAN information
+    $CurrentInterface = New-Object PSObject
+
+    # Parse and add interface details to the object
+    foreach ($line in $lines) {
+        $key, $value = $line -split ":\s+", 2
+
+        if ($key -and $value -and ($key -notlike "*Hosted network status*")) {
+            $CurrentInterface | Add-Member -MemberType NoteProperty -Force -Name $key.Trim() -Value $value.Trim()
+        }
+    }
+
+    # Get WLAN profile information
+    $WLANProfile = (netsh.exe wlan show profiles name="$($CurrentInterface.Profile)" key=clear | Select-String "Key Content" | Get-Unique)
+
+    # Parse and add WLAN profile details to the object
+    $key, $value = $WLANProfile -split ":\s+", 2
+
+    if ($key -and $value) {
+        $CurrentInterface | Add-Member -MemberType NoteProperty -Name $key.Trim() -Value $value.Trim()
+    }
+
+    # Return the object with WLAN information
+    return $CurrentInterface
+}
+
+change_SSID
+cleanup_profiles
