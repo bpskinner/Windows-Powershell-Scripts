a076d9e9cd055a1166475b75de7871b9a86c95a1
Benjamin P. Skinner <68821301+bpskinner@users.noreply.github.com>
2025-01-16 13:03:05 -0500

    Create rename-computer-mac.ps1

---
+++ Active_Directory/rename-computer-mac.ps1
@@ -0,0 +1,76 @@
+#!PS
+
+$state       = "TX"
+$prefix      = "ALLHYU"
+$domain_user = "admin"
+$domain_pass = "password"
+
+<#
+.SYNOPSIS
+Renames computer using the format:
+      "TXALLHYU-8B5D51"
+       |   |      |
+      /    |       \
+     |     |        |
+   "StateStore-MacAddress"
+ [2 Chars][6 to 7 Chars]-[6 Chars]
+
+ State must equal 2      characters
+ Store must equal 6 to 7 characters
+ Dash  must equal 1      characters
+ Mac   must equal 5 to 6 characters 
+
+.DESCRIPTION
+Please fill out the state and prefix using the correct information provided by the corporate team.
+Do not modify any other parameters of the script while running.
+Additionally, please verify that ANY computer you run scripts on are not hosting any services. I.e. Servers, Matrix/Oil machines...
+The length of the Mac Address chosen will be automatically determined using the length of the store prefix. 
+
+.NOTES
+01/16/2025
+#>
+
+$u = $domain_user
+$p = $domain_pass | ConvertTo-SecureString -AsPlainText -Force
+$dcred = [PsCredential]::New($u, $p)
+
+function parse_name ($nic) { 
+    $parsed_mac = ($nic.MacAddress.replace("-","")[($prefix.Length)..12] -join '')
+    $_ = "$($state)$($prefix)-$($parsed_mac)"
+    
+    Write-host "Adapter found -> $($nic.Name) 
+              `nDescription   -> $($nic.InterfaceDescription) 
+              `nAddress       -> $($nic.MacAddress) ($parsed_mac)
+              `nUpdated Name  -> $_"
+
+    return $_
+}
+
+function parse_nics () {
+
+    $nics = Get-NetAdapter
+    
+    $nics | % {
+        if (
+            $_.Status -eq 'Up' `
+            -and $_.MediaType -match '802.3' `
+            -and $_.Virtual -eq $false
+        ) { 
+            return parse_name($_)
+        }
+    }
+    
+    $nics | % {
+        if (
+            $_.Status -eq 'Up' `
+            -and $_.MediaType -match '802.11' `
+            -and ($_.InterfaceDescription -match "Wi[\-]{0,1}Fi|Wireless" -or $_.Name -match "Wi[\-]{0,1}Fi|Wireless")
+        ) {
+            return parse_name($_)
+        }
+    }
+    
+}
+
+$name = parse_nics
+Rename-Computer -NewName $name -DomainCredential $dcred
